e3017e3588ad2a6d96e24f47843cb725f925ffa5
SunnySparks <Josediazt6@outlook.com>
2021-10-05 16:37:45 -0500

    Install webpack

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// import _ from 'lodash';
+import './style.css';
+
+/*
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['it', 'worked'], ' ');
+    element.classList.add('hello');
+  
+    return element;
+  }
+  
+
+
+  window.onload = function() {
+    component()
+    document.body.appendChild(component());
+  };
+  */
