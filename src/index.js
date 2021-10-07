ca90ce7ee83eb00cfebd05741ed53b02641306d1
SunnySparks <Josediazt6@outlook.com>
2021-10-07 13:41:51 -0500

    initial api fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.css';
+
+
+const result = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1/scores/'
+
+var form = document.getElementById('form');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('click');
+
+    var datos = new FormData(form);
+    console.log(datos);
+    console.log(datos.get('user'));
+    console.log(datos.get('score'));
+
+    fetch(result, {
+        method: 'POST',
+        body: datos
+    })
+    .then( res => res.json())
+    .then( data =>  {
+        console.log(data);
+    })
+});
+
+/*
+fetch(result, {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "score": "225",
+        "user": "Pepito"
+    }
+})
+.then(response => response.json() )
+.then(data => {
+    console.log(data);
+})
+.catch(err=>console.log(err))*/
