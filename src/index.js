598419cb458d185a723c0f8bf8ee062ef5e02194
SunnySparks <Josediazt6@outlook.com>
2021-10-08 14:23:31 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,77 +5,71 @@ const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/game
 const wrapper = document.getElementById('leaderboard-cont');
 
 function searchID(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 async function fillRank() {
-    try {
-        const fetching = await fetch(url);
-        const res = await fetching.json();
-        const data = res;
-        let result = Object.entries(data);
-        let leaderboard = data.result;
-        leaderboard.forEach((element, index) => {
-            var liElement = document.createElement('li');
-            var liDiv = document.createElement('div');
-            var liParagraph = document.createElement('p');
-    
-            if (index % 2 == 0){
-                liDiv.classList.add('p-1');
-                liParagraph.innerHTML += `
+  try {
+    const fetching = await fetch(url);
+    const res = await fetching.json();
+    const data = res;
+    const leaderboard = data.result;
+    leaderboard.forEach((element, index) => {
+      const liElement = document.createElement('li');
+      const liDiv = document.createElement('div');
+      const liParagraph = document.createElement('p');
+
+      if (index % 2 === 0) {
+        liDiv.classList.add('p-1');
+        liParagraph.innerHTML += `
                     ${element.user}: 
-                    ${element.score}`
-            }else{
-                liDiv.classList.add('bg-light', 'p-1');
-                liParagraph.innerHTML += `
+                    ${element.score}`;
+      } else {
+        liDiv.classList.add('bg-light', 'p-1');
+        liParagraph.innerHTML += `
                     ${element.user}: 
-                    ${element.score}`
-            }
-            
-            liDiv.appendChild(liParagraph);
-            liElement.appendChild(liDiv);
-            wrapper.appendChild(liElement);
-    
-        });
-    } catch(error) {
-        throw new Error(error.message);
-}
+                    ${element.score}`;
+      }
+
+      liDiv.appendChild(liParagraph);
+      liElement.appendChild(liDiv);
+      wrapper.appendChild(liElement);
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
-async function sendToApi(){
-    const datos = new FormData(form);
-    const user = searchID('user').value;
-    const score = searchID('score').value;
-    let userData = { user, score }
+async function sendToApi() {
+  const user = searchID('user').value;
+  const score = searchID('score').value;
+  const userData = { user, score };
 
-    try {
-        await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify(userData)
-        })
-    } catch(error) {
-        throw new Error(error.message);
-}
-       
+  try {
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(userData),
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 const refresh = document.getElementById('refresh');
-refresh.addEventListener('click', function(e){
-    fillRank();
-})
-
-const form = document.getElementById('form');
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    wrapper.innerHTML = '';
-    sendToApi();
-    fillRank();
+refresh.addEventListener('click', () => {
+  wrapper.innerHTML = '';
+  fillRank();
 });
 
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  wrapper.innerHTML = '';
+  sendToApi();
+  fillRank();
+});
 
-window.onload = function() {
-    fillRank();
-};
+fillRank();
